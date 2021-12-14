ad1f912311b9a6606b119448aafa8f04b1f5973b
Luis Perdigao <perdigao1@yahoo.com>
2021-12-14 23:07:03 +0000

    Update createEXE.bat

--- createEXE.bat
+++ createEXE.bat
@@ -5,6 +5,10 @@ echo REIKNAPATH = %REIKNAPATH%
 
 rem Create correct pyinstaller spec file
 
-pyi-makespec TDCT_main.py --name "3DCorrelationToolbox-RFI" -i "icons\3DCT_icon.ico" --windowed --hidden-import="skimage.filters.rank.core_cy_3d" --add-data "TDCT_correlation.ui;." --add-data "TDCT_main.ui;." --add-data "icons_rc.py;." --add-data "%REIKNAPATH%;reikna"
+rem Windowed
+rem pyi-makespec TDCT_main.py --name "3DCorrelationToolbox-RFI" -i "icons\3DCT_icon.ico" --windowed --hidden-import="skimage.filters.rank.core_cy_3d" --add-data "TDCT_correlation.ui;." --add-data "TDCT_main.ui;." --add-data "icons_rc.py;." --add-data "%REIKNAPATH%;reikna"
 
-pyinstaller 3DCorrelationToolbox-RFI.spec
+rem Not windowed
+pyi-makespec TDCT_main.py --name "3DCorrelationToolbox-RFI" -i "icons\3DCT_icon.ico" --hidden-import="skimage.filters.rank.core_cy_3d" --add-data "TDCT_correlation.ui;." --add-data "TDCT_main.ui;." --add-data "icons_rc.py;." --add-data "%REIKNAPATH%;reikna"
+
+pyinstaller --clean --noconfirm 3DCorrelationToolbox-RFI.spec

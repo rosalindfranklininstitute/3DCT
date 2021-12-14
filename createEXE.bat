f1441f5a754a4cc40c46cee30d57a3a8ac959c58
Luis Perdigao <perdigao1@yahoo.com>
2021-12-14 20:52:45 +0000

    New batch file to create standalone executable

---
+++ createEXE.bat
@@ -0,0 +1,10 @@
+rem Get the location of reikna
+FOR /F "tokens=*" %%i IN ('python -c "import importlib; print(importlib.util.find_spec('reikna').submodule_search_locations[0])" ') do set REIKNAPATH=%%i
+
+echo REIKNAPATH = %REIKNAPATH%
+
+rem Create correct pyinstaller spec file
+
+pyi-makespec TDCT_main.py --name "3DCorrelationToolbox-RFI" -i "icons\3DCT_icon.ico" --windowed --hidden-import="skimage.filters.rank.core_cy_3d" --add-data "TDCT_correlation.ui;." --add-data "TDCT_main.ui;." --add-data "icons_rc.py;." --add-data "%REIKNAPATH%;reikna"
+
+pyinstaller 3DCorrelationToolbox-RFI.spec
